7eb397cecd21426b9da85f1cf64e04b162adccda
bhagyaj <bhagyajayashani@gmail.com>
2018-05-03 20:38:05 +0530

    project structure implemented

---
+++ src/main/java/com/industry/project/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.industry.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class DemoApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
